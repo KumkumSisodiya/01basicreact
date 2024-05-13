11a76d8ced9a9e5da54df683576341033c8774fd
kumkum sisodiya <kumkumsisodiya1137@gmail.com>
2024-05-13 11:44:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+// In component  function always first letter Capital
 import './App.css';
+import React , { useState } from 'react';
+import About from './components/About';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 
 function App() {
+ let [Counter , setCounter] =  useState(15) //counter -:variable , setCounter-; function
+  // let counter = 15
+  const addValue = () =>{
+//  counter = counter + 1;
+console.log('Clicked' , Counter)
+setCounter(Counter + 1)
+ console.log("Succesfully", Counter)
+  }
+  const remove = () => {
+    setCounter(Counter-1)
+    if(Counter < 1){
+      console.log('not possible')
+    }
+    else{
+       console.log('continue')
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+  
+    <h1>Chai aur react</h1>
+     <h2>Counter value: {Counter}</h2>
+     <button onClick={addValue}>
+    Add value{Counter}
+     </button>
+     <button onClick={remove}>remove value{Counter}</button>
+     <About/>
+    </>
   );
 }
 
---
+++ src/components/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Chai or react</h2>
+    )
+}
+export default Chai
