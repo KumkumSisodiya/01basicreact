5b3d479c50d67b3af679b87947ed84c81f1e247d
kumkumsisodiya <113821895+KumkumSisodiya@users.noreply.github.com>
2024-07-11 12:31:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,47 @@ function App() {
   // const [color, setColor] = useState('');
 
   return (
+   import { useEffect, useState } from "react";
+import "./App.css";
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
+import { ThemeProvider } from './context/theme';
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // Apply theme changes to HTML element
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark');
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
     <>
       {/* color changes
       <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
