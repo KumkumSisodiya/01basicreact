5e5dca6b964c05ec5f40c5d439954f857e522e69
kumkum sisodiya <kumkumsisodiya1137@gmail.com>
2024-07-19 06:50:56 +0530

    Merge branch 'master' of https://github.com/kumkumgithub/01basicreact

--- src/App.js
+++ src/App.js
@@ -1,142 +1,18 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { TodoProvider } from "./context/TodoContext";
-import { json } from "react-router-dom";
-import TodoForm from "./components/TodoForm";
-import TodoItem from "./components/TodoItem";
+import React from 'react';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const addTodo = (todo) => {
-    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
-  };
-  const updateTodo = (id, updatedTodo) => {
-    setTodos((prev) =>
-      prev.map((prevTodo) =>
-        prevTodo.id === id ? { ...prevTodo, ...updatedTodo } : prevTodo
-      )
-    );
-  };
+  return (
   
-  const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id));
-  };
-  const toggleComplete = (id) => {
-    setTodos((prev) =>
-      prev.map((prevTodo) =>
-        prevTodo.id === id
-          ? { ...prevTodo, completed: !prevTodo.completed }
-          : prevTodo
-      )
-    );
-  };
-
-  useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"));
-
-    if (todos && todos.length > 0) setTodos(todos);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-  return (
-<<<<<<< HEAD
-    <TodoProvider
-      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
-    >
-      <div>
-        <div className="bg-[#172842] min-h-screen py-8">
-          <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-            <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-              Manage Your Todos
-            </h1>
-            <div className="mb-4">
-              <TodoForm/>
-              </div>
-            <div className="flex flex-wrap gap-y-3">
-              {todos.map((todo) => (
-                <div key={todo.id}
-                  className='w-full'
-                  >
-                    <TodoItem todo={todo}
-                    />
-                  </div>
-              ))}
-            </div>
-=======
-   import { useEffect, useState } from "react";
-import "./App.css";
-import ThemeButton from "./components/ThemeButton";
-import Card from "./components/Card";
-import { ThemeProvider } from './context/theme';
-
-function App() {
-  const [themeMode, setThemeMode] = useState("light");
-
-  const lightTheme = () => {
-    setThemeMode("light");
-  };
-
-  const darkTheme = () => {
-    setThemeMode("dark");
-  };
-
-  // Apply theme changes to HTML element
-  useEffect(() => {
-    document.querySelector('html').classList.remove('light', 'dark');
-    document.querySelector('html').classList.add(themeMode);
-  }, [themeMode]);
-
-  return (
-    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeButton />
-          </div>
-          <div className="w-full max-w-sm mx-auto">
-            <Card />
-          </div>
-        </div>
-      </div>
-    </ThemeProvider>
+    <div>
+    
+      <h1>Learn hello</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  
   );
 }
 
-export default App;
-
-    <>
-      {/* color changes
-      <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
-        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <button
-              onClick={() => setColor('red')}
-              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
-              style={{ backgroundColor: 'red' }}
-            >
-              Red
-            </button>
-            <button
-              onClick={() => setColor('green')}
-              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
-              style={{ backgroundColor: 'green' }}
-            >
-              Green
-            </button>
-            <button
-              onClick={() => setColor('orange')}
-              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
-              style={{ backgroundColor: 'orange' }}
-            >
-              Orange
-            </button>
->>>>>>> 5b3d479c50d67b3af679b87947ed84c81f1e247d
-          </div>
-        </div>
-      </div>
-    </TodoProvider>
-  );
-}
 export default App;
